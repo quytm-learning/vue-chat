17b0fba2a527747e81481142a9e15b7eac3b6781
quytm <minhquylt95@gmail.com>
2018-09-28 10:36:38 +0700

    create base component for Register and Login

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
     mode: 'history',
@@ -19,7 +19,17 @@ export default new Router({
             // route level code-splitting
             // this generates a separate chunk (about.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+            component: () => import(/* webpackChunkName: "about" */ './views/About')
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: () => import(/* webpackChunkName: "about" */ './views/Register')
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import(/* webpackChunkName: "about" */ './views/Login')
         }
     ]
 })
---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="login-layout">
+        <h1>Login</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+
+</style>
